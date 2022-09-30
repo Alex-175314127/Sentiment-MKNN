79330c3d8c3ab4dfcf186e04d3e623dfd31544bf
Alex-175314127 <alexanderadam636@gmail.com>
2022-09-30 16:26:30 +0700

    Revision v30

--- kfold_test.py
+++ kfold_test.py
@@ -12,20 +12,40 @@ from sklearn.model_selection import cross_val_score
 
 #X, y = load_iris(return_X_y=True)
 
-df = pd.read_csv('output\Clean_text_Sentiment.csv')
+df = pd.read_csv('Twitter_fresh\sentiment_result_200.csv')
 X = df['text'].values
 y = df['Sentiment'].values
 
 n = 5
-k=3
+k=12
 kfold = KFold(n, shuffle=True, random_state=33)
 i=1
 sum_akurasi = 0
+acc, rc ,pr, f1 = [],[],[],[]
 
 #tf = TfidfVectorizer(decode_error='replace', lowercase=False)
 enc = LabelEncoder()
 
 #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+def calculate_conf_metrics(y_test, pred):
+    TP, FP, TN, FN = 0,0,0,0
+    for i in range(len(pred)):
+        if (pred[i] == 0) & (y_test[i] == 0):
+            TP += 1
+        elif (pred[i] == 0) & (y_test[i] == 1):
+            FP += 1
+        elif (pred[i] == 1) & (y_test[i] == 1):
+            TN += 1
+        else:
+            FN += 1
+    print(f'TP : {TP} \t FP : {FP} \t TN : {TN} \t FN : {FN}')
+    accuracy = (TP + TN) / (TP + FP + TN + FN)
+    precision = (TP) / (TP + FP)
+    recall = (TP) / (TP + FN)
+    f1_score = (2 * precision * recall) / (precision + recall)
+    
+    return accuracy, precision, recall, f1_score
 
 for train_index, test_index in kfold.split(X):
     print("fold", i)
@@ -39,21 +59,25 @@ for train_index, test_index in kfold.split(X):
     X_train = tf.fit_transform(X_train)
     X_test = tf.transform(X_test)
 
-    #enc = LabelEncoder()
-    #y_train = enc.fit_transform(y_train)
-    #y_test = enc.transform(y_test)
+    enc = LabelEncoder()
+    y_train = enc.fit_transform(y_train)
+    y_test = enc.transform(y_test)
 
-    clf = ModifiedKNN(k)
+    clf = ModifiedKNN(12)
     clf.fit(X_train, y_train)
-    predict = clf.predict(X_test)
+    predict, jarak = clf.predict(X_test)
 
-    conf_mat = confusion_matrix(y_test, predict)
-   #a,b,c,d = conf_mat.ravel()
-    #acc = (a+b) / (a+b+c+d) * 100
-    acc = accuracy_score(y_test, predict)*100
-    sum_akurasi += acc
-    print("Accuracy :", str("%.4f" % acc)+'%')
+    accuracy, precision, recall, f1_score = calculate_conf_metrics(y_test, predict)
+    sum_akurasi += accuracy
+    acc.append(accuracy)
+    pr.append(precision)
+    rc.append(recall)
+    f1.append(f1_score)
+    print("Accuracy :", str("%.4f" % accuracy)+'%')
     i += 1
 
 sum_acc = sum_akurasi/n
 print("Avearge accuracy : ", str("%.4f" % sum_acc)+'%')
+
+print('Accuracy \t Precision\t Recall \t  F1 Score ')
+print(f'{acc} \t\t {pr} \t\t {rc} \t\t {f1}')
--- test_mknn.py
+++ test_mknn.py
@@ -12,7 +12,7 @@ from heapq import nsmallest as nMin
 #X, y = load_iris(return_X_y=True)
 from sklearn.neighbors import NearestNeighbors
 
-df = pd.read_csv('output/Clean_text_Sentiment.csv')
+df = pd.read_csv('Twitter_fresh\sentiment_result_200.csv')
 X = df['text']
 y = df['Sentiment']
 
@@ -25,14 +25,14 @@ tf = TfidfVectorizer()
 X_train = tf.fit_transform(X_train)
 X_test = tf.transform(X_test)
 y_train = enc.fit_transform(y_train)
-nilai_k = 3
+nilai_k = 12
 clf = ModifiedKNN(k=nilai_k)
 clf.fit(X_train, y_train)
 predict, jarak =  clf.predict(X_test)
 #print(X_train)
 
-y_test = y_test.apply(lambda x: 1 if 'Positive' else 0)
-acc = accuracy_score(y_test, predict)*100
+y_test = enc.transform(y_test)
+#acc = accuracy_score(y_test, predict)*100
 #print("ACC :",acc)
 
 #print(predict)
@@ -47,7 +47,31 @@ with open('jarak tetangga.txt', 'w') as f, open('label pred.txt', 'w') as g:
 #print('\n'.join(str(el) for el in jarak))
 
 neigbor_index = clf.get_neigbors(X_test)
-print("index Tetangga = ", neigbor_index)
+#print("index Tetangga = ", neigbor_index)
 
 svN = open('output/test_index.txt', 'w')
 svN.write('\n'.join(str(a) for a in neigbor_index))
+
+def calculate_conf_metrics(y_test, pred):
+    TP, FP, TN, FN = 0,0,0,0
+    for i in range(len(pred)):
+        if (pred[i] == 0) & (y_test[i] == 0):
+            TP += 1
+        elif (pred[i] == 0) & (y_test[i] == 1):
+            FP += 1
+        elif (pred[i] == 1) & (y_test[i] == 1):
+            TN += 1
+        else:
+            FN += 1
+    print(f'TP : {TP} \t FP : {FP} \t TN : {TN} \t FN : {FN}')
+    accuracy = (TP + TN) / (TP + FP + TN + FN)
+    precision = (TP) / (TP + FP)
+    recall = (TP) / (TP + FN)
+    f1_score = (2 * precision * recall) / (precision + recall)
+    
+    return accuracy, precision, recall, f1_score
+
+
+accuracy, precision, recall, f1_score = calculate_conf_metrics(y_test, predict)
+print('Accuracy \t Precision\t Recall \t  F1 Score ')
+print(f'{accuracy} \t\t {precision} \t\t {recall} \t\t {f1_score}')
